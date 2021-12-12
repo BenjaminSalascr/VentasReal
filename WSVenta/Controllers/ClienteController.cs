d5215faf5f997acf85f99ffe3934ccefb2ba2943
JORGE ALEXANDER ARDILA RESTREPO <jorge.ardila1641@correo.policia.gov.co>
2021-12-12 17:34:30 -0500

    se creo el CRUD de cliente en el WS

--- WSVenta/Controllers/ClienteController.cs
+++ WSVenta/Controllers/ClienteController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Linq;
 using WSVenta.Models;
+using WSVenta.Models.Request;
+using WSVenta.Models.Response;
 
 namespace WSVenta.Controllers
 {
@@ -11,11 +14,86 @@ namespace WSVenta.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-            using (VentaRealContext db = new VentaRealContext())
+            Respuesta respuesta = new Respuesta();
+            try
             {
-                var lst = db.Cliente.ToList();
-                return Ok(lst);
+                using (VentaRealContext db = new VentaRealContext())
+                {
+                    var lst = db.Cliente.ToList();
+                    respuesta.Exito = 1;
+                    respuesta.Data = lst;
+                }
             }
+            catch (Exception ex)
+            {
+                respuesta.Mensaje= ex.Message;
+            }
+            return Ok(respuesta);
+        }
+
+        [HttpPost]
+        public IActionResult Add(ClienteRequest model)
+        {
+            Respuesta respuesta = new Respuesta();
+            try
+            {
+                using (VentaRealContext db = new VentaRealContext())
+                {
+                    var cliente = new Cliente();
+                    cliente.Nombre = model.Nombre;
+                    db.Cliente.Add(cliente);
+                    db.SaveChanges();
+                    respuesta.Exito = 1;                    
+                }
+            }
+            catch (Exception ex)
+            {
+                respuesta.Mensaje = ex.Message;
+            }
+            return Ok(respuesta);
+        }
+
+        [HttpPut]
+        public IActionResult Edit(ClienteRequest model)
+        {
+            Respuesta respuesta = new Respuesta();
+            try
+            {
+                using (VentaRealContext db = new VentaRealContext())
+                {
+                    var cliente = db.Cliente.Find(model.Id);
+                    cliente.Nombre = model.Nombre;
+                    db.Entry(cliente).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                    db.SaveChanges();
+                    respuesta.Exito = 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                respuesta.Mensaje = ex.Message;
+            }
+            return Ok(respuesta);
+        }
+
+        [HttpDelete("{Id}")]
+        public IActionResult Delete(int Id)
+        {
+            Respuesta respuesta = new Respuesta();
+            try
+            {
+                using (VentaRealContext db = new VentaRealContext())
+                {
+                    var cliente = db.Cliente.Find(Id);
+                    db.Remove(cliente);
+                    db.SaveChanges();
+                    respuesta.Exito = 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                respuesta.Mensaje = ex.Message;
+            }
+            return Ok(respuesta);
         }
     }
 }
