3068d9e3006346703928992cb3aae516ec0be678
JORGE ALEXANDER ARDILA RESTREPO <jorge.ardila1641@correo.policia.gov.co>
2021-12-12 09:48:06 -0500

    Agregamos nuggets, mapeamos toda la bd con scaffolding,  y creamos el metodo get de la tabla clientes en el controlador Cliente del WS API REST

---
+++ WSVenta/Controllers/ClienteController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using WSVenta.Models;
+
+namespace WSVenta.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            using (VentaRealContext db = new VentaRealContext())
+            {
+                var lst = db.Cliente.ToList();
+                return Ok(lst);
+            }
+        }
+    }
+}
