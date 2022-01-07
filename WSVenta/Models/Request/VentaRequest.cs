c9f7802560e2a3df7edb498aa6f31837ef1fd840
jaardila-3 <jorge.ardila1641@correo.policia.gov.co>
2022-01-06 21:38:03 -0500

    validaciones attribute de propiedades en backend en VentaRequest

--- WSVenta/Models/Request/VentaRequest.cs
+++ WSVenta/Models/Request/VentaRequest.cs
@@ -1,12 +1,19 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace WSVenta.Models.Request
 {
     public class VentaRequest
     {
+        [Required]
+        [Range(1, double.MaxValue, ErrorMessage = "El valor del IdCliente debe ser mayor a cero")]
+        [ExisteCliente(ErrorMessage = "El cliente no existe")]
         public int IdCliente { get; set; }
-        public decimal Total { get; set; }
+
+        [Required]
+        [MinLength(1, ErrorMessage = "Deben existir conceptos")]
         public List<ConceptoRequest> Conceptos { get; set; }
+
         public VentaRequest()
         {
             this.Conceptos = new List<ConceptoRequest>();
@@ -21,4 +28,19 @@ namespace WSVenta.Models.Request
         public decimal Importe { get; set; }
         public int IdProducto { get; set; }
     }
+
+    #region Validaciones Attribute
+    public class ExisteCliente : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            int idCliente = (int)value;
+            using (var db = new VentaRealContext())
+            {
+                if(db.Cliente.Find(idCliente) == null) return false;
+            }
+            return true;
+        }
+    }
+    #endregion
 }
--- WSVenta/Models/VentaRealContext.cs
+++ WSVenta/Models/VentaRealContext.cs
@@ -30,7 +30,7 @@ namespace WSVenta.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=OFITE-GRUDE8\\SQLEXPRESS;Database=VentaReal;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("Server=localhost;Database=VentaReal;Trusted_Connection=True;");
             }
         }
 
